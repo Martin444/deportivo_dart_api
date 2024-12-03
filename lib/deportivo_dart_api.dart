26e69840a040025590ee8cdb9f75605c34641a80
mfarel <mfarel@openpass.com.ar>
2024-12-02 22:54:04 -0300

    fix exports

--- lib/deportivo_dart_api.dart
+++ lib/deportivo_dart_api.dart
@@ -2,3 +2,4 @@ library deportivo_dart_api;
 
 export 'package:deportivo_dart_api/core/api.dart';
 export 'package:deportivo_dart_api/core/exeptions/api_exception.dart';
+export 'package:deportivo_dart_api/features/auth/login/data/usescase/login_usescase.dart';
