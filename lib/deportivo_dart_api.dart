0d62eafde991c5f74d50a4e1b9f22b88826ac35a
mfarel <mfarel@openpass.com.ar>
2024-12-07 20:24:39 -0300

    add usescase

--- lib/deportivo_dart_api.dart
+++ lib/deportivo_dart_api.dart
@@ -3,4 +3,6 @@ library deportivo_dart_api;
 export 'package:deportivo_dart_api/core/api.dart';
 export 'package:deportivo_dart_api/core/exeptions/api_exception.dart';
 export 'package:deportivo_dart_api/features/auth/login/data/usescase/login_usescase.dart';
+export 'package:deportivo_dart_api/features/auth/login/model/user_succes_model.dart';
+export 'package:deportivo_dart_api/features/auth/register/data/usescase/register_commerce_usescase.dart';
 export 'package:deportivo_dart_api/features/upload_images/upload_images.dart';
